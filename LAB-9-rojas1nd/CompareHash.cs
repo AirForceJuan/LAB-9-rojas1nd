cade220ec8efd5430268bd8cca6cfaa56b1bd608
AirForceJuan <31664858+AirForceJuan@users.noreply.github.com>
2017-11-02 17:51:00 -0400

    start of brute force. needs work but its agood start.

--- LAB-9-rojas1nd/CompareHash.cs
+++ LAB-9-rojas1nd/CompareHash.cs
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Security.Cryptography;
 
-
 namespace LAB_9_rojas1nd
 {
-    class Program
+    class CompareHash 
     {
-        public static IEnumerable<string> Generate(int len, char[] Allowed_chars)
+        
+
+
+        public CompareHash() { }
+
+       
+        public static IEnumerable<string> Generate(int len,char[] Allowed_chars)
         {
-            if (len == 1)
+            if(len == 1)
             {
                 foreach (char c in Allowed_chars)
                 {
@@ -26,33 +31,15 @@ namespace LAB_9_rojas1nd
 
                 foreach (char c in Allowed_chars)
                 {
-                    foreach (string sub in sub_strings)
+                    foreach(string sub in sub_strings)
                     {
-                        Console.WriteLine(GetHash.Hash(c + sub) + " " +c+sub);
+                        
                         yield return c + sub;
                     }
                 }
             }
         }
+      
 
-
-
-        static void Main(string[] args)
-        {
-            List<string> HashedPasswords = File.ReadLines("C:\\words.txt").ToList();
-
-
-
-
-            string Alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-
-            List<string> result = Generate(8, Alphabet.ToCharArray()).ToList();
-
-
-
-
-        
-        }
     }
 }
---
+++ LAB-9-rojas1nd/GetHash.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace LAB_9_rojas1nd
+{
+    class GetHash
+    {
+        public GetHash() { }
+
+        public static string Hash(string temp)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.ASCII.GetBytes(temp));
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
