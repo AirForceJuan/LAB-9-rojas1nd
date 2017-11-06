a3542c3011fbe12d7b6c2511561f1893a186eb9b
AirForceJuan <31664858+AirForceJuan@users.noreply.github.com>
2017-11-06 17:21:23 -0500

    got it working so that it can find more than one hash per program run,

--- LAB-9-rojas1nd/CompareHash.cs
+++ LAB-9-rojas1nd/CompareHash.cs
@@ -5,41 +5,53 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Security.Cryptography;
+using System.Threading;
 
 namespace LAB_9_rojas1nd
 {
-    class CompareHash 
+    class CompareHash
     {
-        
-
-
-        public CompareHash() { }
-
-       
-        public static IEnumerable<string> Generate(int len,char[] Allowed_chars)
+        public static bool toggle = false;
+        public static IEnumerable<string> Generate(int len, char[] Allowed_chars, string TheOne)
         {
-            if(len == 1)
-            {
-                foreach (char c in Allowed_chars)
+                if (len == 1)
                 {
-                    yield return c.ToString();
-                }
-            }
-            else
-            {
-                var sub_strings = Generate(len - 1, Allowed_chars);
-
-                foreach (char c in Allowed_chars)
-                {
-                    foreach(string sub in sub_strings)
+                    foreach (char c in Allowed_chars)
                     {
-                        
-                        yield return c + sub;
+                        yield return c.ToString();
                     }
                 }
-            }
-        }
-      
+                else
+                {
+                    var sub_strings = Generate(len - 1, Allowed_chars, TheOne);
 
+                    foreach (char c in Allowed_chars)
+                    {
+                        foreach (string sub in sub_strings)
+                        {
+                            string hashcheck = (GetHash.Hash(c + sub));
+                            if (hashcheck == TheOne)
+                            {
+                            Console.WriteLine(TheOne + " == " + (c + sub));
+                            CompareHash.toggle = true;
+                            yield return c+sub;
+                            
+                            }
+
+                            yield return c + sub;
+                        }
+
+                        if (toggle)
+                        {
+                            yield break;
+                        }
+                    }
+                    
+                }   
+
+       
+
+        }
     }
 }
+
--- LAB-9-rojas1nd/Program.cs
+++ LAB-9-rojas1nd/Program.cs
@@ -5,54 +5,62 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Security.Cryptography;
+using System.Threading;
 
 
 namespace LAB_9_rojas1nd
 {
     class Program
     {
-        public static IEnumerable<string> Generate(int len, char[] Allowed_chars)
-        {
-            if (len == 1)
-            {
-                foreach (char c in Allowed_chars)
-                {
-                    yield return c.ToString();
-                }
-            }
-            else
-            {
-                var sub_strings = Generate(len - 1, Allowed_chars);
-
-                foreach (char c in Allowed_chars)
-                {
-                    foreach (string sub in sub_strings)
-                    {
-                        Console.WriteLine(GetHash.Hash(c + sub) + " " +c+sub);
-                        yield return c + sub;
-                    }
-                }
-            }
-        }
+        
 
 
 
         static void Main(string[] args)
         {
+            
+
+            
+
             List<string> HashedPasswords = File.ReadLines("C:\\words.txt").ToList();
 
 
+            
 
 
             string Alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+           
 
-            List<string> result = Generate(8, Alphabet.ToCharArray()).ToList();
+            string test_1 = HashedPasswords[0];
+            string test_2 = HashedPasswords[1];
+            string test_3 = HashedPasswords[2];
+            string test_4 = HashedPasswords[3];
+            string test_5 = HashedPasswords[4];
+            string test_6 = HashedPasswords[5];
+
+
+
+            List<string> result_1 = CompareHash.Generate(8, Alphabet.ToCharArray(), test_1).ToList();
+            CompareHash.toggle = false;
+            List<string> result_2 = CompareHash.Generate(8, Alphabet.ToCharArray(), test_2).ToList();
+            CompareHash.toggle = false;
+            List<string> result_3 = CompareHash.Generate(8, Alphabet.ToCharArray(), test_3).ToList();
+            //CompareHash.toggle = false;
+            //List<string> result_4 = CompareHash.Generate(8, Alphabet.ToCharArray(), test_4).ToList(); //This crashed my Gaming Computer.... No Idea what it is?!?!?!?!
+            CompareHash.toggle = false;
+            List<string> result_5 = CompareHash.Generate(8, Alphabet.ToCharArray(), test_5).ToList();
+            CompareHash.toggle = false;
+            List<string> result_6 = CompareHash.Generate(8, Alphabet.ToCharArray(), test_6).ToList();
 
 
 
 
-        
+
+
+            Console.WriteLine("You Are Done");
+
+            Console.Read();
         }
     }
 }
